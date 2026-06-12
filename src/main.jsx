7fa8060666b62812d614f51f72566dc7ad388df4
devbalbuena <balbuenadexter2@gmail.com>
2026-06-12 12:33:29 +0800

    add a two new features SEO and opentags and vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App.jsx';
 import { ThemeProvider } from './context/ThemeContext.jsx';
 import { ToastProvider } from './context/ToastContext.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <ToastProvider>
         <App />
+        <Analytics />
       </ToastProvider>
     </ThemeProvider>
   </StrictMode>,
